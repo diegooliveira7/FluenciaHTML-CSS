8941054f912778cfbe9cedad124ad914c4b85f3f
Diego <diegoolliveira1999@gmail.com>
2021-02-15 22:33:06 -0300

    Create file aula_web.html and configuration of  aula_html.html

--- testejava.java
+++ testejava.java
@@ -4,6 +4,6 @@
 public class testejava {
 
     public static void main(String[] args) {
-        System.out.println("Bom dia");
+        System.out.println("Bom dia");//Comando para executar o código ctr alt n
     }
 }
